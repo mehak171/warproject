d988e5ce232850874b0b9d2ef0cd541ddafdea60
Mehakdeep Singh Dhaliwal <dhalimeh@sheridancollege.ca>
2024-04-09 23:43:52 -0400

    final changes

---
+++ SYST17796_ProjectStarterCode/test/ca/sheridancollege/project/DeckTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mehak
+ */
+public class DeckTest {
+    
+    public DeckTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    @Test
+    public void testSize() {
+        System.out.println("size");
+         Deck instance = new Deck();
+    assertEquals("Newly created deck should have 52 cards", 52, instance.size());
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+   
+    
+}
